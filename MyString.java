e47441d1a46c6dda337d3b8c71d97416709c406b
Sophie Nichol <snichol00@stuy.edu>
2018-11-25 22:46:59 -0500

    finished subsequence

--- MyString.java
+++ MyString.java
@@ -18,17 +18,21 @@ public class MyString implements CharSequence,Comparable<CharSequence>{
 
   //finds length of string
   public int length(){
-
+    return data.length;
   }
 
   // implements like substring
   public String subSequence(int start, int end){
-
+    String output = "";
+    for (; start < end; start++){
+      output += data[start];
+    }
+    return output;
   }
 
   // converts back to a string in order to return
   public String toString(){
-
+    
   }
 
   //returns 1 if the same, -1 otherwise? I don't really understand
